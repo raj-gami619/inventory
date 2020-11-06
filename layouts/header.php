ee094b6702be03a52ee6f23683208db435fb69ec
Raj Gami <raj.gami@strathmore.edu>
2020-11-06 09:57:12 +0300

    Update header.php

--- layouts/header.php
+++ layouts/header.php
@@ -19,7 +19,8 @@
       <div class="logo pull-left"> EAISL - Inventory </div>
       <div class="header-content">
       <div class="header-date pull-left">
-        <strong><?php echo date("F j, Y, g:i a");?></strong>
+
+        <strong><?php date_default_timezone_set('Africa/Nairobi'); echo date("F j, Y, g:i a");?></strong>
       </div>
       <div class="pull-right clearfix">
         <ul class="info-menu list-inline list-unstyled">
